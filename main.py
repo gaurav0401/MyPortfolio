2ba79b213db2ba68b403a180c315b75022befe1e
gaurav_04_01 <gaurav.tnv6970@gmail.com>
2023-06-19 00:04:55 +0530

    version 1.3

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask , render_template
+
+app=Flask(__name__ , template_folder='Template' , static_folder='StaticFiles')
+
+@app.route('/')
+
+def  home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
